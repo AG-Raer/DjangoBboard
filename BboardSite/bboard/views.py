451f8a9e81663352eee5cb345971f70adf36ae21
Artem <agachin03@gmail.com>
2025-07-16 14:41:45 +0300

    Added detail view for announcements

--- BboardSite/bboard/views.py
+++ BboardSite/bboard/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Bb
 from .forms import BbForm
-
 
 def index(request):
     bbs = Bb.objects.all()
     return render(request, 'bboard/index.html', {'bbs': bbs})
-
 
 def create(request):
     if request.method == 'POST':
@@ -18,7 +16,9 @@ def create(request):
         form = BbForm()
     return render(request, 'bboard/create.html', {'form': form})
 
-
+def detail(request, bb_id):
+    bb = get_object_or_404(Bb, pk=bb_id)
+    return render(request, 'bboard/detail.html', {'bb': bb})
 from django.shortcuts import render
 
 # Create your views here.
