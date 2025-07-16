869b4f7bbbba1626045b27cf396c32abded7e16f
Artem <agachin03@gmail.com>
2025-07-16 15:00:05 +0300

    Added image field to Bb model

--- BboardSite/bboard/models.py
+++ BboardSite/bboard/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-
 class Bb(models.Model):
     title = models.CharField(max_length=50, verbose_name='Заголовок')
     content = models.TextField(verbose_name='Описание')
     price = models.FloatField(verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    image = models.ImageField(upload_to='images/', verbose_name='Изображение', blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -14,4 +14,3 @@ class Bb(models.Model):
         verbose_name = 'Объявление'
         verbose_name_plural = 'Объявления'
         ordering = ['-published']
-
