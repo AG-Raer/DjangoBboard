f9af226dcff78b93ae2488d30eeea1b871018a43
Artem <agachin03@gmail.com>
2025-07-16 14:47:31 +0300

    Added detail route for announcements

--- BboardSite/bboard/urls.py
+++ BboardSite/bboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
+    path('bboard/<int:bb_id>/', views.detail, name='detail'),
 ]
