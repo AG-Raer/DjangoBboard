03494d362c4a469b4bf7c273dccdb5dcf4909c87
Artem <agachin03@gmail.com>
2025-07-16 13:46:11 +0300

    Added Bb model for announcements

--- BboardSite/bboard/models.py
+++ BboardSite/bboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Bb(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Описание')
+    price = models.FloatField(verbose_name='Цена')
+    published = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+        ordering = ['-published']
+
