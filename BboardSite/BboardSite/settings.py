57effe88ea690e017648bac64e216d906560f55a
Artem <agachin03@gmail.com>
2025-07-16 15:09:26 +0300

    Configured static files settings

--- BboardSite/BboardSite/settings.py
+++ BboardSite/BboardSite/settings.py
@@ -126,3 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Static files
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
