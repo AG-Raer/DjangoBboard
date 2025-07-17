f4d0336d681c3f50558de7f7e122f3a901c437db
Artem <agachin03@gmail.com>
2025-07-17 19:12:23 +0300

    Updated admin to display user field

--- BboardSite/bboard/admin.py
+++ BboardSite/bboard/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django import forms
+from .models import Bb
 
-# Register your models here.
+class BbAdminForm(forms.ModelForm):
+    class Meta:
+        model = Bb
+        fields = '__all__'
+
+    def clean_slug(self):
+        slug = self.cleaned_data['slug']
+        if slug:
+            return slugify(slug, allow_unicode=True) or slug
+        return slug
+
+@admin.register(Bb)
+class BbAdmin(admin.ModelAdmin):
+    form = BbAdminForm
+    list_display = ['title', 'slug', 'price', 'published', 'user']
+    list_filter = ['published', 'user']
+    search_fields = ['title', 'content']
+    fields = ['title', 'content', 'price', 'image', 'slug', 'user']
