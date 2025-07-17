23c04283a50c2f24896311d97741148e1640e4f9
Artem <agachin03@gmail.com>
2025-07-17 18:30:19 +0300

    Restored DEBUG=True after 500 test

--- BboardSite/BboardSite/settings.py
+++ BboardSite/BboardSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$3%q5d@43=zek%%c!&v8#@#pcbpjdx!+k%638vu6oqa(e1$hl*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -115,7 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -126,7 +128,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
-# Static files
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
