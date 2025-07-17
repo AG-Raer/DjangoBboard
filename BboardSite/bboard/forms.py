00da1d092e1fcb3adf5b8a9190884e6e32037e96
Artem <agachin03@gmail.com>
2025-07-17 18:49:50 +0300

    Added CustomUserCreationForm for registration

--- BboardSite/bboard/forms.py
+++ BboardSite/bboard/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Bb
 
 class BbForm(forms.ModelForm):
@@ -11,3 +13,16 @@ class BbForm(forms.ModelForm):
             'price': 'Цена',
             'image': 'Изображение',
         }
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, help_text='Введите действительный email.')
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Этот email уже зарегистрирован.')
+        return email
